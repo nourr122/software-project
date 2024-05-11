1c7d2bcd6c7d175760e1c276e88a7efd7c26e3ef
nourr122 <152524706+nourr122@users.noreply.github.com>
2024-05-10 22:58:43 -0700

    Update _layout.jsx

--- project/app/account/_layout.jsx
+++ project/app/account/_layout.jsx
@@ -4,27 +4,27 @@
 //   return <Tabs />;
 // }
 
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { Stack, useRouter } from 'expo-router'
+// import { View, Text, Button } from 'react-native'
+// import React from 'react'
+// import { Stack, useRouter } from 'expo-router'
 
 
-export default function _layout() {
-    const router = useRouter();
+// export default function _layout() {
+//     const router = useRouter();
 
-  return (
-    <Stack screenOptions={{
-        headerStyle : {
-          backgroundColor: 'white'
-        },
-        headerTintColor: 'black'
-      }}>
+//   return (
+//     <Stack screenOptions={{
+//         headerStyle : {
+//           backgroundColor: 'white'
+//         },
+//         headerTintColor: 'black'
+//       }}>
    
-    <Stack.Screen name="login" options={{ headerTitle: 'Login' }} />
-    <Stack.Screen name="register" options={{ headerTitle: 'Create Account' }} />
-    <Stack.Screen name="forgotpass" options={{ headerTitle: 'Forgot Password'}} />
+//     <Stack.Screen name="login" options={{ headerTitle: 'Login' }} />
+//     <Stack.Screen name="register" options={{ headerTitle: 'Create Account' }} />
+//     <Stack.Screen name="forgotpass" options={{ headerTitle: 'Forgot Password'}} />
 
    
-  </Stack>
-  )
-}
+//   </Stack>
+//   )
+// }
